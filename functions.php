237d73162599e3ca67c4fbc577e9cf39fe796185
aumoraes <aumoraes@sp.r7.com>
2017-08-07 16:47:12 -0300

    configurando projeto

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Aumoraes-profile Theme
+ * Mokaeventos
  *
  * Seguimos o padrão de desenvolvimendo criado pelo WordPress:
  *
@@ -8,57 +8,10 @@
  * @link https://make.wordpress.org/core/handbook/best-practices/inline-documentation-standards/php/
  *
  * @package WordPress
- * @subpackage aumoraes-profile
+ * @subpackage mokaeventos
  */
 
- /**
-	* Register Values to translate in all the site managed in admin polylang
-	* Add here all the word and frases to translate.
-	*/
 
-
-
-pll_register_string( 'about', 'about', 'menu' );
-
-pll_register_string( 'description', 'description', 'header' );
-pll_register_string( 'header-desktop', 'header-desktop', 'header' );
-pll_register_string( 'header-mobile', 'header-mobile', 'header' );
-
-pll_register_string( 'resume', 'resume', 'About' );
-pll_register_string( 'resume_link', 'resume_link', 'About' );
-pll_register_string( 'experience', 'experience', 'experience' );
-pll_register_string( 'current', 'current', 'experience' );
-
-
-// add_action( 'wp_head', 'get_current_language' );
-//
-// function get_current_language() {
-//
-// 	$translations = pll_current_language( 'slug' );
-// 	set_background($translations);
-//
-// }
-
-
-
-/**
- * Adiciona os styles e scripts necessários para o tema
- */
-// function set_background($translations = "br") {
-// 	if( $translations === 'en'){
-// 		$translations = 'us';
-// 	}
-// 	wp_enqueue_style( 'style-background-image-'.$translations, get_template_directory_uri() . '/assets/css/'.$translations.'_background.css' );
-// }
-
-// Our filter callback function
-// function skoob_book( ) {
-// 		return "<script type='text/javascript' src='https://www.skoob.com.br/widget/estante/68202/4574466/'> </script>";
-// }
-// add_filter( 'skoob_filter', 'skoob_book');
-//
-// $value = apply_filters( 'skoob_filter', 'skoob_book' );
-// echo $value;
 
 
 /**
@@ -77,6 +30,6 @@ function aumoraes_widgets_init() {
 	) );
 
 }
-add_action( 'widgets_init', 'aumoraes_widgets_init' );
+//add_action( 'widgets_init', 'aumoraes_widgets_init' );
 
 show_admin_bar( false );
