31e51d05d09f31dba560588d8fe44c0c72d9873f
aumoraes <aumoraes@sp.r7.com>
2017-10-21 10:31:31 -0200

    Alterando botão de contato que fica no footer

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 
 ?>
 <?php
-include_once "footer-contato-mobile.php";
+//include_once "footer-contato-mobile.php";
 ?>
 
 
@@ -72,6 +72,27 @@ include_once "footer-contato-mobile.php";
 <script src="<?php echo get_stylesheet_directory_uri();?>/assets/gallery/lightgallery-all.min.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri();?>/assets/gallery/jquery.mousewheel.min.js"></script>
 
+
+<!-- WhatsHelp.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            facebook: "1957550641236197", // Facebook page ID
+            whatsapp: "+5511965271419", // WhatsApp number
+            company_logo_url: "//scontent.xx.fbcdn.net/v/t1.0-1/p50x50/22046577_1959902527667675_6156946559554393852_n.png?oh=bcecd16b33589ef728cbadf44eeded05&oe=5A6E66C2", // URL of company logo (png, jpg, gif)
+            greeting_message: "Olá, como podemos ajudar? Deixe uma mensagem que vamos entrar em contato.", // Text of greeting message
+            call_to_action: "Envie uma  mensagem", // Call to action
+            button_color: "#960303", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp" // Order of buttons
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /WhatsHelp.io widget -->
 
 </body>
 
