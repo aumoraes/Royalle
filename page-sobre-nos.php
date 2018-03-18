5545c133b10603c6cc32c3b04a00992c5448921e
aumoraes <aumoraes@sp.r7.com>
2018-03-18 14:42:29 -0300

    ajustando pagina home

--- page-sobre-nos.php
+++ page-sobre-nos.php
@@ -9,11 +9,11 @@
   <div class="container">
     <div class="about">
       <p class="description">
-        A Royalle Eventos é uma empresa criada em Outubro de 2017 com o objetivo que levar uma proposta diferente para as festas. Com atendimento no local da festa, conseguimos atender festa em casa, buffet, sítio e empresa.
+        A Royalle Eventos é uma empresa criada em Outubro de 2017 com o objetivo que levar uma proposta diferente para as festas. Com atendimento no local da festa, conseguimos atender eventos em casa, buffet, sítio e empresa.
         Com o conceito de preparação de alimentos muitas vezes direto no salão torna esse processo interessante onde os convidados podem ver como os alimentos são preparados.
-        Nossa equipe conta com 2 pessoas fixas e ajuda de varios outros colaboradoes.
-        Assim como qualquer outro empreendimento que trabalha como o manusei de alimentos, achamos que o principal na hora de prepará-los é a higiene, depois vem o sabor e a aparencia. Alguns de nossos lanches como a pizza e os x-burgers não tem conservante e são totalmente preparados por nós.
-        Todo o comando do preparado dos alimentos fica por conta do Gabriel, o nosso expecialista no assunto com vários anos trabalhando nessa área.
+        Nossa equipe conta com 2 pessoas fixas e ajuda de vários outros colaboradoes.
+        Assim como qualquer outro empreendimento que trabalha como o manuseio de alimentos, acreditamos que o principal na hora de prepará-los é a higiêne, depois vem o sabor e a aparencia. Alguns de nossos lanches como a pizza e os x-burgers não tem conservante e são totalmente preparados por nós.
+        Todo o comando do preparado dos alimentos fica por conta do Gabriel, o nosso especialista no assunto com vários anos trabalhando nessa área.
       </p>
     </div>
 
@@ -31,7 +31,7 @@
               Nossas Barracas
             </h2>
             <p class="description">
-              Temos barracas personalizadas e customizadas para atender nossos clientes e levar charme até o evento. Conseguimos preparar e servir as nossas melhores opções de forma dinâmica e estilo.
+              Temos barracas personalizadas e customizadas para atender nossos clientes e levar charme até o evento. Conseguimos preparar e servir as nossas melhores opções de forma dinâmica e com estílo.
             </p>
           </div>
         </div>
@@ -76,7 +76,7 @@
       </div>
     </div>
     <div class="team">
-      <h2 class="page-title">Nosso time</h2>
+      <h2 class="page-title">Resposáveis</h2>
     </div>
   </div>
 </div>
--- template-parts/sobre.php
+++ template-parts/sobre.php
@@ -14,7 +14,7 @@
 		<div class="description">
 			<h3>A Melhor Escolha</h3>
 			<p>
-			A <a href="http://royalleeventos.com.br" target="_self"><strong>Royalle Eventos</strong></a>, <strong>barraquinhas em casa</strong> leva até você o dinamismo da gastronomia com alimentos preparados na hora, o que faz com que os alimentos sejam servidos quentinhos e saborosos. Os nossos colaboradores tem o prazer de mostrar o preparo dos lanchinhos com todo carinho. Gostamos de ressaltar nossas <strong>Mini Pizzas</strong> e nossos <strong>Mini X-Burger</strong>  pois não tem conservantes.
+			A <a href="http://royalleeventos.com.br" target="_self"><strong>Royalle Eventos</strong></a>, <strong>barraquinhas em casa</strong> leva até você o dinamismo da gastronomia com alimentos preparados na hora, o que faz com que os alimentos sejam servidos quentinhos e saborosos. Os nossos colaboradores tem o prazer de mostrar o preparo dos lanchinhos com todo carinho. Gostamos de ressaltar nossa <strong>Mini Pizza</strong> e nosso <strong>Mini X-Burger</strong>  pois não tem conservantes.
 			Para <strong>festas temáticas</strong> também podemos adaptar as <strong>barraquinhas</strong> para entrar no clima da sua festa, como <strong>festa junina</strong> e outros.
 			Faça um orçamento sem compromisso e fique de olho nas <strong>promoções</strong>.
 			</p>
